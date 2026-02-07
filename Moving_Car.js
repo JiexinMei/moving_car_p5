aae68b1cbeeba63c01040e926c44780fdea334fa
JiexinMei <jiexin3803@gmail.com>
2026-02-07 18:02:13 +0000

    mao

--- Moving_Car.js
+++ Moving_Car.js
@@ -1,7 +1,7 @@
 // Car position variable
 let carX = 0;
-let carY;
-let carSpeed = 3;
+let carY=0;
+let carSpeed = 10;
 
 function setup() {
   createCanvas(800, 400);
@@ -13,8 +13,26 @@ function draw() {
   // ===== DRAW BACKGROUND =====
   // Sky (light blue)
   background(135, 206, 235);
+fill('grey')
+rect(255, 110, 20, 200)
+fill('yellow')
+rect(145, 15, 260, 160)
+fill('red')
+rect(150, 20, 250, 150)
+ellipse(50, 50, 80, 80)
+fill('yellow')
+textAlign(LEFT, CENTER);
+  textFont('roboto');
+  textSize(37);
+text('China is number one', 190, 100, 250);
+textAlign(LEFT, CENTER);
+  textFont('roboto');
+  textSize(20);
+fill('red')
+text('Red Sun in the Sky  (Mao Zedong)', 15, 150, 100);
 
-  // ===== BEGIN BILLBOARD HERE =====
+
+
   
   // ===== END BILLBOARD HERE =====
 
@@ -49,11 +67,11 @@ function draw() {
 // Function to draw the car
 function drawCar(x, y) {
   // Car body (red)
-  fill(255, 0, 0);
+  fill(50, 50, 50);
   rect(x, y, 100, 40, 5);
 
   // Car top (red)
-  fill(200, 0, 0);
+  fill(55, 55, 55);
   rect(x + 20, y - 25, 60, 30, 5);
 
   // Windows (light blue)
@@ -74,4 +92,12 @@ function drawCar(x, y) {
   // Headlight (yellow)
   fill(255, 255, 0);
   circle(x + 95, y + 15, 8);
+fill(255, 0, 0);
+  rect(x, y + 15, 6);
+
+fill('green')
+textSize(48);
+text('grass (real)', 200, 350, 4000);
+
+
 }
